23ee3c144a536fbac1fb0ab077a685e13e148353
7 <ruuuziiir@gmail.com>
2021-01-02 18:21:36 +0800

    first

--- whatcaneat/ViewController.swift
+++ whatcaneat/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var optionSeg: UISegmentedControl!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    func randomChoose() {
+        
+        if optionSeg.selectedSegmentIndex == 0 {
+            foodImage.image = UIImage(named: "beef")
+        } else if optionSeg.selectedSegmentIndex == 1 {
+            foodImage.image = UIImage(named: "chicken")
+        } else if optionSeg.selectedSegmentIndex == 2 {
+            foodImage.image = UIImage(named: "veggie")
+        } else {
+            foodImage.image = UIImage(named: "pork")
+        }
     }
 
+    @IBAction func random(_ sender: Any) {
+        
+        optionSeg.selectedSegmentIndex = Int.random(in: 0...3)
+        randomChoose()
 
+
+    }
+    
+    @IBAction func optionPic(_ sender: Any) {
+        
+    randomChoose()
+    }
+    
+    
 }
 
