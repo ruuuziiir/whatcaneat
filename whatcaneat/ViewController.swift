aad110823045ac09e643954fe5c204805b1f24bd
7 <ruuuziiir@gmail.com>
2021-01-02 16:28:35 +0800

    Initial Commit

---
+++ whatcaneat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whatcaneat
+//
+//  Created by 7 on 2021/1/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
